b55bcc8337a61a0d23500f8ce15d205b5d316d71
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-07-17 14:15:00 +0200

    refactor(parser): Rename hex and dec parsing

--- src/parser/address/relative.rs
+++ src/parser/address/relative.rs
@@ -2,9 +2,6 @@ use super::{Address, Res};
 
 use nom::{branch::alt, bytes::complete::take_while, error::context};
 
-use decimal::parse as dec_parse;
-use hexadecimal::parse as hex_parse;
-
 mod decimal;
 mod hexadecimal;
 
@@ -19,7 +16,7 @@ fn parse_relative_symbolic(input: &str) -> Res<&str, Address> {
 pub fn parse_relative(input: &str) -> Res<&str, Address> {
     context(
         "Relative address",
-        alt((hex_parse, dec_parse, parse_relative_symbolic)),
+        alt((hexadecimal::parse, decimal::parse, parse_relative_symbolic)),
     )(input)
     .map(|(next_input, address)| (next_input, address))
 }
